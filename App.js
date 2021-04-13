15b36889fd84379f71a9d44f3404db8f253e970c
Muhammad Jaafer Riaz <16-te-20@students.uettaxila.edu.pk>
2021-04-13 18:40:44 +0500

    third commit

--- App.js
+++ App.js
@@ -1,126 +1,255 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {Text, View, StyleSheet, Button, TouchableWithoutFeedback,Pressable} from 'react-native';
+
+const array = [['', '', ''],['', '', ''],['', '', '']];
 
 export default function App() {
-  const randomnum=()=>{
-    var num= Math.floor(Math.random() *100);
-    ;
-    return num;
-    //return random num
-  }
-  const [getText, setText] = useState("")
-  const [getguess,message]=useState("")
-  const[actguess,mes]=useState(randomnum())
-  const[roundno,setround]=useState(0)
+  const [getBtnTxt1, setBtnTxt1] = useState('-');
+  const [getBtnTxt2, setBtnTxt2] = useState('-');
+  const [getBtnTxt3, setBtnTxt3] = useState('-');
+  const [getBtnTxt4, setBtnTxt4] = useState('-');
+  const [getBtnTxt5, setBtnTxt5] = useState('-');
+  const [getBtnTxt6, setBtnTxt6] = useState('-');
+  const [getBtnTxt7, setBtnTxt7] = useState('-');
+  const [getBtnTxt8, setBtnTxt8] = useState('-');
+  const [getBtnTxt9, setBtnTxt9] = useState('-');
+  const [getResult, setResult] = useState('');
+  const [getTurn, setTurn] = useState('p1');
 
-  console.log(actguess);
-  const buttonClick = (txt) => {
-    // alert(txt)
-    setText(getText +txt)
-  }
-  const resetBtn = (txt) => {
-    // alert(txt)
-    setText("")
-    message("")  
-  }
-   const checkguess=()=>{
-     setround(roundno+1);
-    if(getText==actguess){
-      message("Guess is correct");
+  const onClick = (id) => {
+    if (getTurn == 'p1') {
+      if (id == 1 && getBtnTxt1 == '-') {
+        setBtnTxt1('X');
+        array[0][0] = 'X';
+      }
+      if (id == 2 && getBtnTxt2 == '-') {
+        setBtnTxt2('X');
+        array[0][1] = 'X';
+      }
+      if (id == 3 && getBtnTxt3 == '-') {
+        setBtnTxt3('X');
+        array[0][2] = 'X';
+      }
+      if (id == 4 && getBtnTxt4 == '-') {
+        setBtnTxt4('X');
+        array[1][0] = 'X';
+      }
+      if (id == 5 && getBtnTxt5 == '-') {
+        setBtnTxt5('X');
+        array[1][1] = 'X';
+      }
+      if (id == 6 && getBtnTxt6 == '-') {
+        setBtnTxt6('X');
+        array[1][2] = 'X';
+      }
+      if (id == 7 && getBtnTxt7 == '-') {
+        setBtnTxt7('X');
+        array[2][0] = 'X';
+      }
+      if (id == 8 && getBtnTxt8 == '-') {
+        setBtnTxt8('X');
+        array[2][1] = 'X';
+      }
+      if (id == 9 && getBtnTxt9 == '-') {
+        setBtnTxt9('X');
+        array[2][2] = 'X';
+      }
+      setTurn('p2');
+    } else if (getTurn == 'p2') {
+      if (id == 1 && getBtnTxt1 == '-') {
+        setBtnTxt1('O');
+        array[0][0] = 'O';
+      }
+      if (id == 2 && getBtnTxt2 == '-') {
+        setBtnTxt2('O');
+        array[0][1] = 'O';
+      }
+      if (id == 3 && getBtnTxt3 == '-') {
+        setBtnTxt3('O');
+        array[0][2] = 'O';
+      }
+      if (id == 4 && getBtnTxt4 == '-') {
+        setBtnTxt4('O');
+        array[1][0] = 'O';
+      }
+      if (id == 5 && getBtnTxt5 == '-') {
+        setBtnTxt5('O');
+        array[1][1] = 'O';
+      }
+      if (id == 6 && getBtnTxt6 == '-') {
+        setBtnTxt6('O');
+        array[1][2] = 'O';
+      }
+      if (id == 7 && getBtnTxt7 == '-') {
+        setBtnTxt7('O');
+        array[2][0] = 'O';
+      }
+      if (id == 8 && getBtnTxt8 == '-') {
+        setBtnTxt8('O');
+        array[2][1] = 'O';
+      }
+      if (id == 9 && getBtnTxt9 == '-') {
+        setBtnTxt9('O');
+        array[2][2] = 'O';
+      }
+      setTurn('p1');
     }
-    else{
-      message("Guess is incorrect");
+    console.log(array);
+    result();
+  };
+  const result = () => {
+    if (
+      (array[0][0] === 'X' && array[0][1] === 'X' && array[0][2] === 'X') ||
+      (array[0][0] === 'X' && array[1][1] === 'X' && array[2][2] === 'X') ||
+      (array[0][0] === 'X' && array[1][0] === 'X' && array[2][0] === 'X') ||
+      (array[0][1] === 'X' && array[1][1] === 'X' && array[2][1] === 'X') ||
+      (array[0][2] === 'X' && array[1][2] === 'X' && array[2][2] === 'X') ||
+      (array[0][2] === 'X' && array[1][1] === 'X' && array[2][0] === 'X') ||
+      (array[0][2] === 'X' && array[1][1] === 'X' && array[2][0] === 'X') ||
+      (array[1][0] === 'X' && array[1][1] === 'X' && array[1][2] === 'X') ||
+      (array[2][0] === 'X' && array[2][1] === 'X' && array[2][2] === 'X')
+    ) {
+      setTurn('')
+      setResult('PLAYER 1(X) HAS WON!');
+    } else if (
+      (array[0][0] === 'O' && array[0][1] === 'O' && array[0][2] === 'O') ||
+      (array[0][0] === 'O' && array[1][1] === 'O' && array[2][2] === 'O') ||
+      (array[0][0] === 'O' && array[1][0] === 'O' && array[2][0] === 'O') ||
+      (array[0][1] === 'O' && array[1][1] === 'O' && array[2][1] === 'O') ||
+      (array[0][2] === 'O' && array[1][2] === 'O' && array[2][2] === 'O') ||
+      (array[0][2] === 'O' && array[1][1] === 'O' && array[2][0] === 'O') ||
+      (array[0][2] === 'O' && array[1][1] === 'O' && array[2][0] === 'O') ||
+      (array[1][0] === 'O' && array[1][1] === 'O' && array[1][2] === 'O') ||
+      (array[2][0] === 'O' && array[2][1] === 'O' && array[2][2] === 'O')
+    ) {
+      setTurn('')
+      setResult('PLAYER 2(O) HAS WON!');
+    } else if (
+      array[0][0] !== '' &&
+      array[0][1] !== '' &&
+      array[0][2] !== '' &&
+      array[1][0] !== '' &&
+      array[1][1] !== '' &&
+      array[1][2] !== '' &&
+      array[2][0] !== '' &&
+      array[2][1] !== '' &&
+      array[2][2] !== ''
+    ) {
+      setTurn('')
+      setResult('DRAW!');
     }
-  }
-  
+  };
+
+  const play = () => {
+    setBtnTxt1('-'),setBtnTxt2('-'),setBtnTxt3('-'),setBtnTxt4('-'),setBtnTxt5('-'),setBtnTxt6('-'),setBtnTxt7('-'),setBtnTxt8('-'),setBtnTxt9('-');
+    array[0][0]= '', array[0][1]= '', array[0][2]= '', array[1][0]= '', array[1][1]= '', array[1][2]= '', array[2][0]= '', array[2][1]= '', array[2][2] = '';
+    setResult('');
+    setTurn('p1');
+  };
+
+
   return (
     <View style={styles.container}>
-      
-      <Text style={{ fontSize: 20 }}>Guess game by Danish Ali</Text>
-
-      <Text style={{ fontSize: 20 }}>{getText}</Text>
-      
-      
-      <Text style={{ fontSize: 20 }}>{getguess}</Text>
-      
-      
-      
-      <Text style={{ fontSize: 20 }}>{roundno}</Text>
-      
-      
-     <View style={inputbutton.btn}>  
-    <View style={inputbutton.btnspace}>
-      <Button title= "0" onPress={buttonClick.bind(this, 0)}> </Button>
+    <View style={[{ padding: '10%', alignItems: 'center' }]}>
+        <Text style={[{  fontSize: '25', justifyContent: 'center' }]}/>
+        TURN : {getResult == '' ? (getTurn==('p1') ? 'PLAYER 1 (X)' : 'PLAYER 2 (O)') : ''}
+        <Text />
       </View>
-    <View style={inputbutton.btnspace}>
-      <Button title="1"  onPress={buttonClick.bind(this, 1)}> </Button> 
-       </View> 
-    <View style={inputbutton.btnspace}>
-      <Button title="2" onPress={buttonClick.bind(this, 2)}> </Button>
+      <View style={styles.viewRowContainer}>
+        <Pressable onPress={() => onClick(1)} style={[{backgroundColor:'#eee'}]}>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+              {getBtnTxt1}
+            </Text>
+          </View>
+        </Pressable>
+        <Pressable onPress={() => onClick(2)}>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+              {getBtnTxt2}
+            </Text>
+          </View>
+        </Pressable>
+        <Pressable onPress={() => onClick(3)}>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+              {getBtnTxt3}
+            </Text>
+          </View>
+        </Pressable>
       </View>
-     </View>
-
-     <View style={inputbutton.btn}>  
-    <View style={inputbutton.btnspace}>
-      <Button title= "3" onPress={buttonClick.bind(this, 3)}> </Button>
+      <View style={styles.viewRowContainer}>
+        <Pressable onPress={() => onClick(4)}>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+              {getBtnTxt4}
+            </Text>
+          </View>
+        </Pressable>
+        <Pressable onPress={() => onClick(5)}>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+              {getBtnTxt5}
+            </Text>
+          </View>
+        </Pressable>
+        <Pressable onPress={() => onClick(6)}>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+              {getBtnTxt6}
+            </Text>
+          </View>
+        </Pressable>
       </View>
-    <View style={inputbutton.btnspace}>
-      <Button title="4"  onPress={buttonClick.bind(this, 4)}> </Button> 
-       </View> 
-    <View style={inputbutton.btnspace}>
-      <Button title="5" onPress={buttonClick.bind(this, 5)}> </Button>
+      <View style={styles.viewRowContainer}>
+        <Pressable onPress={() => onClick(7)}>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+              {getBtnTxt7}
+            </Text>
+          </View>
+        </Pressable>
+        <Pressable onPress={() => onClick(8)}>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+              {getBtnTxt8}
+            </Text>
+          </View >
+        </Pressable>
+        <Pressable onPress={() => onClick(9)}>
+          <View style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+              {getBtnTxt9}
+            </Text>
+          </View>
+        </Pressable>
       </View>
-     </View>
+      <View style={[{ alignItems: 'center' }]}>
+        <Text style={[{ padding: '10%', fontSize: '25', justifyContent: 'center' }]}/>
+        {getResult}
+        <Text />
+      </View>
+      <View style={[{ padding: '10%', fontSize: '25' }]}>
+        <Button onPress={play} title={'Play Again'} />
+      </View>
+      <Pressable onPress={()=>{
+alert('hello');
+      }}>
+      <Text>Helo click</Text>
       
-     <View style={inputbutton.btn}>  
-    <View style={inputbutton.btnspace}>
-      <Button title= "6" onPress={buttonClick.bind(this, 6)}> </Button>
-      </View>
-    <View style={inputbutton.btnspace}>
-      <Button title="7"  onPress={buttonClick.bind(this, 7)}> </Button> 
-       </View> 
-    <View style={inputbutton.btnspace}>
-      <Button title="8" onPress={buttonClick.bind(this, 8)}> </Button>
-      </View>
-     </View>
-       
-    <View style={inputbutton.btn}>  
-      
-      <View style={inputbutton.btnspace}>
-      <Button title="9"  onPress={buttonClick.bind(this, 9)}> </Button> 
-       </View> 
-      
-      <View style={inputbutton.btnspace}>
-      <Button title="ENTER" onPress={checkguess.bind(this)}> </Button>
-      </View>
-      <View style={inputbutton.btnspace}>
-      <Button title="RESET" onPress={resetBtn.bind(this,0)}> </Button>
-      </View>
-
-      </View> 
-
+      </Pressable>
     </View>
   );
 }
 
-const inputbutton = StyleSheet.create({
-  btn:{
-    flexDirection: 'row',
-    marginBottom: 15, 
-  },
-  btnspace:{
-    marginRight: 15, 
-    width: 90,
-  }
-});
-
 const styles = StyleSheet.create({
-    container: {
-      flex:1,      
-      backgroundColor: 'pink',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1 ',
+    padding: 8,
+  },
+  viewRowContainer: { flexDirection: 'row', justifyContent: "space-between" },
+  buttonText: { fontSize: 50 },
+  buttonContainer: { width: '100%', height: 100, borderColor: 'black', borderWidth: 1, justifyContent: 'center', alignItems: 'center' },
 });
---
+++ practocejavascript.js
@@ -0,0 +1,126 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const randomnum=()=>{
+    var num= Math.floor(Math.random() *100);
+    ;
+    return num;
+    //return random num
+  }
+  const [getText, setText] = useState("")
+  const [getguess,message]=useState("")
+  const[actguess,mes]=useState(randomnum())
+  const[roundno,setround]=useState(0)
+
+  console.log(actguess);
+  const buttonClick = (txt) => {
+    // alert(txt)
+    setText(getText +txt)
+  }
+  const resetBtn = (txt) => {
+    // alert(txt)
+    setText("")
+    message("")  
+  }
+   const checkguess=()=>{
+     setround(roundno+1);
+    if(getText==actguess){
+      message("Guess is correct");
+    }
+    else{
+      message("Guess is incorrect");
+    }
+  }
+  
+  return (
+    <View style={styles.container}>
+      
+      <Text style={{ fontSize: 20 }}>Guess game by Danish Ali</Text>
+
+      <Text style={{ fontSize: 20 }}>{getText}</Text>
+      
+      
+      <Text style={{ fontSize: 20 }}>{getguess}</Text>
+      
+      
+      
+      <Text style={{ fontSize: 20 }}>{roundno}</Text>
+      
+      
+     <View style={inputbutton.btn}>  
+    <View style={inputbutton.btnspace}>
+      <Button title= "0" onPress={buttonClick.bind(this, 0)}> </Button>
+      </View>
+    <View style={inputbutton.btnspace}>
+      <Button title="1"  onPress={buttonClick.bind(this, 1)}> </Button> 
+       </View> 
+    <View style={inputbutton.btnspace}>
+      <Button title="2" onPress={buttonClick.bind(this, 2)}> </Button>
+      </View>
+     </View>
+
+     <View style={inputbutton.btn}>  
+    <View style={inputbutton.btnspace}>
+      <Button title= "3" onPress={buttonClick.bind(this, 3)}> </Button>
+      </View>
+    <View style={inputbutton.btnspace}>
+      <Button title="4"  onPress={buttonClick.bind(this, 4)}> </Button> 
+       </View> 
+    <View style={inputbutton.btnspace}>
+      <Button title="5" onPress={buttonClick.bind(this, 5)}> </Button>
+      </View>
+     </View>
+      
+     <View style={inputbutton.btn}>  
+    <View style={inputbutton.btnspace}>
+      <Button title= "6" onPress={buttonClick.bind(this, 6)}> </Button>
+      </View>
+    <View style={inputbutton.btnspace}>
+      <Button title="7"  onPress={buttonClick.bind(this, 7)}> </Button> 
+       </View> 
+    <View style={inputbutton.btnspace}>
+      <Button title="8" onPress={buttonClick.bind(this, 8)}> </Button>
+      </View>
+     </View>
+       
+    <View style={inputbutton.btn}>  
+      
+      <View style={inputbutton.btnspace}>
+      <Button title="9"  onPress={buttonClick.bind(this, 9)}> </Button> 
+       </View> 
+      
+      <View style={inputbutton.btnspace}>
+      <Button title="ENTER" onPress={checkguess.bind(this)}> </Button>
+      </View>
+      <View style={inputbutton.btnspace}>
+      <Button title="RESET" onPress={resetBtn.bind(this,0)}> </Button>
+      </View>
+
+      </View> 
+
+    </View>
+  );
+}
+
+const inputbutton = StyleSheet.create({
+  btn:{
+    flexDirection: 'row',
+    marginBottom: 15, 
+  },
+  btnspace:{
+    marginRight: 15, 
+    width: 90,
+  }
+});
+
+const styles = StyleSheet.create({
+    container: {
+      flex:1,      
+      backgroundColor: 'pink',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    
+});
